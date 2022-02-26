6cafe1322a414eaeea865fff234f4358ecbf72ac
loldonut <ricricramisogaming18@gmail.com>
2022-02-26 19:12:25 +0800

    added a helpCommand function for command  help

--- cli.js
+++ cli.js
@@ -8,16 +8,14 @@ const { InvalidFileError } = require('./src/errors');
 
 const [,, ...args] = process.argv;
 
-// Technically I don't have to do this
-// It just looks more nicer to give
-// it a name
 function main() {
     if (!args[0]) return help();
-    if (args[0] === 'help') return help();
+    if (args[0] === 'help') return help(args[1]);
 
     const file = args[0];
     if(!file) throw new InvalidFileError('No File Input');
     let fileContent;
+
     try {
         fileContent = fs.readFileSync(`${file}`);
     } catch {
@@ -31,15 +29,26 @@ function main() {
     console.log(bruhlang.start());
 }
 
-function help() {
-    const infos = [{
-        name: '<file>',
-        description: 'Runs the file given',
-    }, {
-        name: 'help',
-        description: 'Displays this message'
-    }]
+// Information about the Commands
+const infos = [{
+    name: '<file>',
+    description: 'Runs the file given',
+}, {
+    name: 'help [command-name]',
+    raw: 'help',
+    description: 'Displays this message'
+}];
 
+const commandNames = [
+    'help',
+];
+
+function help(arg) {
+    if (arg && commandNames.includes(arg)) return helpCommand(arg);
+    else if (arg && !commandNames.includes(arg)) return console.error(`Command '${arg}' does not exist`);
+    
+    // Combine all the infos
+    // and add 4 spaces per command
     let info = '';
     infos.forEach((data, i) => {
         const needsNewLine = i !== infos.length ? '\n' : '';
@@ -48,7 +57,19 @@ function help() {
     });
 
     console.log(
-        `BruhLang | v${pkgInfo.version}\nCommands:\n${info}`
+        `BruhLang | v${pkgInfo.version}\nCommands:\n${info}`,
+    )
+}
+
+function helpCommand(arg) {
+    const commandInfo = infos.find(x => x.raw === arg);
+    
+    const name = commandInfo.raw;
+    const description = commandInfo.description;
+    const usage = commandInfo.name;
+    
+    console.log(
+        `bruhlang ${name} - ${description}\nUsage:\n    ${usage}`
     )
 }
 
